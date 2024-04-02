f96e60afd4dc0d28dbeb92cac4319066064e197b
Jonathan Victor <jhonathanvictor142@gmail.com>
2024-04-02 13:17:13 -0300

    Ajuste do footer, btn whats

--- apps/main.js
+++ apps/main.js
@@ -1,12 +1,23 @@
 const btnAbrirCarrinho = document.getElementById('abrir-carrinho');
 const btnCarrinhoDetlhePedido = document.querySelector('.container-btn-abrir-carrinho');
-const containerCarrinho = document.querySelector('.container-carrinho')
+const containerCarrinho = document.querySelector('.container-carrinho');
+const btnWhatsDuvidas = document.querySelector('#whatsapp');
 
 btnAbrirCarrinho.addEventListener('click', abrirCarrinho);
-btnCarrinhoDetlhePedido.addEventListener('click', abrirCarrinho)
+btnCarrinhoDetlhePedido.addEventListener('click', abrirCarrinho);
+btnWhatsDuvidas.addEventListener('click', footerAbrirWhatsDuvidas);
 
 function abrirCarrinho() {
     containerCarrinho.classList.remove('hidden');
 };
 
+
+function footerAbrirWhatsDuvidas() {
+    let mensagem = 'Olá, tenho uma dúvida!';    
+
+    const numeroWhatsapp = '82998313115';
+    const url = `https://wa.me/${numeroWhatsapp}?text=${encodeURIComponent(mensagem)}`;
+
+    window.open(url, '_blank');
+};
 
